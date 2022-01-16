b358f86b45977c163ae817d698a417532f5db6b7
Joo <joo8070@gmail.com>
2022-01-16 14:54:36 +0900

    Add project files

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    public int height;
+    public int length;
+
+    bool[,] grid;
+
+    public void Init(int length, int height)
+    {
+        grid = new bool[length, height];
+        this.length = length;
+        this.height = height;
+    }
+}
---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[RequireComponent(typeof(Grid))]
+[RequireComponent(typeof(Tilemap))]
+public class GridManager : MonoBehaviour
+{
+    Tilemap tilemap;
+    Grid grid;
+    [SerializeField] TileBase tileBase;
+
+    void Start()
+    {
+        tilemap = GetComponent<Tilemap>();
+        grid = GetComponent<Grid>();
+        grid.Init(25, 12);
+        UpdateTileMap();
+    }
+
+    void UpdateTileMap()
+    {
+        for (int x = 0; x < grid.length; x++)
+        {
+            for (int y = 0; y < grid.height; y++)
+            {
+                tilemap.SetTile(new Vector3Int(x, y, 0), tileBase);
+            }
+        }
+    }
+}
